57f3d0290c922e2843ccd7076e7717f638dc7776
Slawek Galka <galka.slawek6@gmail.com>
2021-11-13 21:38:45 +0100

    prawie działa

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-var cont = document.createElement('div');
+var amount = window.prompt()
+
+var stringy = (_amount) =>{
+    _amount = amount;
+    string = 'auto ';
+    for (let i = 0; i < _amount - 1; i++) {
+        string = string + 'auto ';
+    }
+    return string;
+}
+
+const cont = document.createElement('div');
 cont.className = 'container';
 document.body.appendChild(cont);
 
-for (let i = 0; i < 256; i++) {
-    var el = document.createElement('div');
+cont.style.cssText = `grid-template-columns: ${stringy()};`;
+
+for (let i = 0; i < Math.pow(amount, 2); i++) {
+    const el = document.createElement('div');
     cont.appendChild(el);
     el.classList.add(`el`, `div-${i+1}`);
 }
 
 document.querySelectorAll('.el').forEach(item => 
     {
-    item.addEventListener('click', event => 
+    item.addEventListener('mouseover', event => 
     {
-        event.target.style.backgroundColor = 'blue';
+        event.target.style.backgroundColor = 'black';
 
         //setTimeout(function() {
         //    event.target.style.backgroundColor = "";
         //  }, 200);
-    }, false)
+    })
     
 })
+
+
   
