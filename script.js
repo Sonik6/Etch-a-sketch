9c1a11f691c350f53ef9e22446dd8dc5178a9e14
Slawek Galka <galka.slawek6@gmail.com>
2022-06-08 21:51:50 +0200

    some tweaks : D

--- script.js
+++ script.js
@@ -1,46 +1,54 @@
-var amount = window.prompt()
+var amount = 0;
 
-var stringy = (_amount) =>{
-    _amount = amount;
-    string = 'auto ';
-    for (let i = 0; i < _amount - 1; i++) {
-        string = string + 'auto ';
+function show() {
+    document.querySelector('.clean').style.display = 'block';
+    document.querySelector('.refresh').style.display = 'block';
+    amount = document.querySelector('#input').value;
+    document.querySelector('.input').style.display = 'none';
+
+    var stringy = (_amount) =>{
+        _amount = amount;
+        string = 'auto ';
+        for (let i = 0; i < _amount - 1; i++) {
+            string = string + 'auto ';
+        }
+        return string;
     }
-    return string;
-}
-
-
-
-const cont = document.createElement('div');
-cont.className = 'container';
-document.body.appendChild(cont);
-
-cont.style.cssText = `grid-template-columns: ${stringy()};`;
-
-for (let i = 0; i < Math.pow(amount, 2); i++) {
-    const el = document.createElement('div');
-    cont.appendChild(el);
-    el.classList.add(`el`, `div-${i+1}`);
-}
-
-document.querySelectorAll('.el').forEach(item => 
-    {
-    item.addEventListener('mouseover', event => 
-    {
-        event.target.style.backgroundColor = 'black';
-
-        //setTimeout(function() {
-        //    event.target.style.backgroundColor = "";
-        //  }, 200);
-    })
     
-})
+    
+    
+    const cont = document.createElement('div');
+    cont.className = 'container';
+    document.body.appendChild(cont);
+    
+    cont.style.cssText = `grid-template-columns: ${stringy()};`;
+    
+    for (let i = 0; i < Math.pow(amount, 2); i++) {
+        const el = document.createElement('div');
+        cont.appendChild(el);
+        el.classList.add(`el`, `div-${i+1}`);
+    }
+    
+    document.querySelectorAll('.el').forEach(item => 
+        {
+       
+            item.addEventListener('mouseover', event => 
+        {
+            event.target.style.backgroundColor = 'black';
+            //setTimeout(function() {
+            //    event.target.style.backgroundColor = "";
+            //  }, 200);
+        })
+    })
+}
+
+
 
 
 
 const refreshPage = () => {
-    document.getElementsByClassName('.el').forEach(item => {
-        item.backgroundColor = 'white';
+    document.querySelectorAll('.el').forEach(item => {
+        item.style.backgroundColor = 'white';
     } )
 }
 
